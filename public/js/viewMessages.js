6ccdd9022433f853f4993482065f33fa3065b5cf
Marcus Bischof <bischofmarcus@gmail.com>
2021-07-21 19:47:46 +0000

    Hello world, first commit.

---
+++ public/js/viewMessages.js
@@ -0,0 +1,28 @@
+const getMessages = () => {
+    console.log("hello getMessages()");
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    findMessage(data);
+    console.log(data);
+    });
+};
+
+const findMessage = (messages) => {
+    const passcodeAttempt = document.querySelector('#passcode').value;
+    for(message in messages) {
+        const messageData = messages[message];
+        if(messageData.passcode === passcodeAttempt) {
+            renderMessageAsHtml(messageData.message);
+        };
+    };
+};
+
+const renderMessageAsHtml = (message) => {
+    // Hide Input Form
+    const passcodeInput = document.querySelector('#passcodeInput');
+    passcodeInput.style.display = 'none';
+    // Render messageas HTML
+    const messageDiv = document.querySelector('#message');
+    messageDiv.innerHTML = message;
+};
